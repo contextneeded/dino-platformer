0deade1f3b576427cf2012bbd80cde22462c0fa5
contextneeded <87148730+contextneeded@users.noreply.github.com>
2021-07-09 20:48:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,12 @@ enum ActionKind {
     Idle,
     Jumping,
     standingRight,
-    standingLeft
+    standingLeft,
+    walkingLeft,
+    walkingRight
 }
 function createStandAnimation () {
-    animStandRight = animation.createAnimation(ActionKind.standingRight, 1000)
+    animStandRight = animation.createAnimation(ActionKind.Walking, 200)
     animStandRight.addAnimationFrame(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . c c c c . . . . 
@@ -43,7 +45,7 @@ function createStandAnimation () {
         . . . c c b 5 5 c c c b b c . . 
         . . . . . c 5 5 d c c c c c . . 
         `)
-    animStandLeft = animation.createAnimation(ActionKind.standingLeft, 1000)
+    animStandLeft = animation.createAnimation(ActionKind.Walking, 200)
     animStandLeft.addAnimationFrame(img`
         . . . . . . . . . . . . . . . . 
         . . . . c c c c . . . . . . . . 
@@ -84,117 +86,6 @@ function createStandAnimation () {
     animation.attachAnimation(dino, animStandLeft)
 }
 function createMap () {
-    dino = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c . . . . 
-        . . . . . . c c 5 5 5 5 c c . . 
-        . . . . . c 5 5 5 5 5 5 5 5 c . 
-        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
-        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
-        . . . c 5 5 5 5 5 5 b 1 b b c c 
-        . . c d 5 5 5 5 5 5 b b 3 3 c c 
-        . . c d d d 5 5 5 5 5 3 3 3 5 c 
-        . . c d d d 5 5 5 5 5 5 5 5 b . 
-        . . c d d d d b 5 5 c b b c . . 
-        c c c d d d d b b 5 5 c b b c . 
-        c d d d d d d d d c c c c c c . 
-        c c d d d b 5 5 d c c c c . . . 
-        . . c c c b 5 5 b c c b c . . . 
-        . . . . . c b 5 5 d c c c . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(dino, 50, 0)
-    dino.ay = 290
-    scene.placeOnRandomTile(dino, 9)
-    scene.cameraFollowSprite(dino)
-}
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    dino.vy = -140
-    if (dino.isHittingTile(CollisionDirection.Bottom)) {
-        dino.vy = -140
-    }
-})
-controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
-    dino.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . c c c c . . . . . . . . 
-        . . c c 5 5 5 5 c c . . . . . . 
-        . c 5 5 5 5 5 5 5 5 c . . . . . 
-        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
-        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
-        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
-        c c b b 1 b 5 5 5 5 5 5 d c . . 
-        c 5 3 3 3 5 5 5 5 5 d d d c . . 
-        . b 5 5 5 5 5 5 5 5 d d d c . . 
-        . . c b b c 5 5 b d d d d c c . 
-        . c b b c 5 5 b b d d d d c d c 
-        . c c c c c c d d d d d d d d c 
-        . . . c c c c d 5 5 b d d d c . 
-        . . c c c c c b 5 5 b c c c . . 
-        . . c b b b c d 5 5 b c . . . . 
-        `)
-})
-scene.onHitTile(SpriteKind.Player, 10, function (sprite) {
-    game.over(true)
-})
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    animation.setAction(dino, ActionKind.Walking)
-})
-function createEggs () {
-    tile_list = scene.getTilesByType(5)
-    for (let value of tile_list) {
-        Egg = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . f f f . . . . . . . . 
-            . . . . f 1 5 5 f . . . . . . . 
-            . . . f 1 5 5 5 5 f . . . . . . 
-            . . . f 5 5 5 5 5 f . . . . . . 
-            . . f 5 5 5 5 5 5 5 f . . . . . 
-            . . f 5 5 5 5 5 5 5 f . . . . . 
-            . . f 5 5 5 5 5 5 5 f . . . . . 
-            . . f 5 5 5 5 5 5 5 f . . . . . 
-            . . f 5 5 5 5 5 5 5 f . . . . . 
-            . . . f 5 5 5 5 5 f . . . . . . 
-            . . . . f f f f f . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Food)
-        scene.place(value, Egg)
-        animation.runMovementAnimation(
-        Egg,
-        animation.animationPresets(animation.bobbing),
-        2000,
-        true
-        )
-    }
-}
-controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
-    dino.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . c c c c . . . . 
-        . . . . . . c c 5 5 5 5 c c . . 
-        . . . . . c 5 5 5 5 5 5 5 5 c . 
-        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
-        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
-        . . . c 5 5 5 5 5 5 b 1 b b c c 
-        . . c d 5 5 5 5 5 5 b b 3 3 c c 
-        . . c d d d 5 5 5 5 5 3 3 3 5 c 
-        . . c d d d 5 5 5 5 5 5 5 5 b . 
-        . . c d d d d b 5 5 c b b c . . 
-        c c c d d d d b b 5 5 c b b c . 
-        c d d d d d d d d c c c c c c . 
-        c c d d d b 5 5 d c c c c . . . 
-        . . c c c b 5 5 b c c b c . . . 
-        . . . . . c b 5 5 d c c c . . . 
-        `)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
-    otherSprite.destroy()
-    music.jumpUp.play()
-})
-function createDino () {
     scene.setBackgroundColor(9)
     scene.setTileMap(img`
         9 9 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -315,11 +206,246 @@ function createDino () {
         . . . . . . . . . . . . . . . . 
         `, false)
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    dino.vy = -140
+    if (dino.isHittingTile(CollisionDirection.Bottom)) {
+        dino.vy = -140
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.setAction(dino, ActionKind.Walking)
+})
+scene.onHitTile(SpriteKind.Player, 10, function (sprite) {
+    game.over(true)
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.setAction(dino, ActionKind.Walking)
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    animation.setAction(dino, ActionKind.Walking)
+})
+function createWalkingAnimation () {
+    animWalkLeft = animation.createAnimation(ActionKind.Walking, 100)
+    animWalkLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . c c c c . . . . . . . . 
+        . . c c 5 5 5 5 c c . . . . . . 
+        . c 5 5 5 5 5 5 5 5 c . . . . . 
+        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . b 5 5 5 5 5 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c c . 
+        . c b b c 5 5 b b d d d d c d c 
+        . c c c c c c d d d d d d d d c 
+        . . . c c c c d 5 5 b d d d c . 
+        . . c c c c c b 5 5 b c c c . . 
+        . . c b b b c d 5 5 b c . . . . 
+        `)
+    animWalkLeft.addAnimationFrame(img`
+        . . . . c c c c c . . . . . . . 
+        . . c c 5 5 5 5 5 c . . . . . . 
+        . c 5 5 5 5 1 f 5 5 c . . . . . 
+        c 5 5 5 5 5 f f 5 5 5 c . . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . c 5 5 5 5 b 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . . c c c b 5 5 b d d d c . 
+        . . . . . c d 5 5 b b c c c . . 
+        . . . . c c c c c c c . . . . . 
+        . . . . c b b b c . . . . . . . 
+        `)
+    animWalkLeft.addAnimationFrame(img`
+        . . . . c c c c c . . . . . . . 
+        . . c c 5 5 5 5 5 c . . . . . . 
+        . c 5 5 5 5 1 f 5 5 c . . . . . 
+        c 5 5 5 5 5 f f 5 5 5 c . . . . 
+        c 5 5 5 5 5 5 5 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c 5 3 3 3 5 5 5 5 5 5 5 d c . . 
+        c 5 5 5 5 5 5 5 5 5 d d d c . . 
+        . c 5 5 5 5 b 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . . c c b 5 5 b d d d c c . 
+        . . . . c d 5 5 b b c c c . . . 
+        . . . . c c c c c c c . . . . . 
+        . . . . c b b b c . . . . . . . 
+        `)
+    animWalkLeft.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . c c c c . . . . . . . . 
+        . . c c 5 5 5 5 c c . . . . . . 
+        . c 5 5 5 5 5 5 5 5 c . . . . . 
+        c 5 5 5 5 5 1 f 5 5 5 c . . . . 
+        c 5 5 5 5 5 f f 5 5 5 5 c . . . 
+        c c b b 1 b 5 5 5 5 5 5 c . . . 
+        c c 3 3 b b 5 5 5 5 5 5 d c . . 
+        c 5 3 3 3 5 5 5 5 5 d d d c . . 
+        . b 5 5 5 5 5 5 5 5 d d d c . . 
+        . . c b b c 5 5 b d d d d c . . 
+        . c b b c 5 5 b b d d d d c c c 
+        . c c c c c c d d d d d d d d c 
+        . . . c c c c d 5 5 b d d d c c 
+        . . . c b c c b 5 5 b c c c . . 
+        . . . c c c d 5 5 b c . . . . . 
+        `)
+    animWalkRight = animation.createAnimation(ActionKind.Walking, 100)
+    animWalkRight.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . c d 5 5 5 5 5 5 b 1 b b c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . c c d d d d b 5 5 c b b c . . 
+        c d c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c d d d b 5 5 d c c c c . . . 
+        . . c c c b 5 5 b c c c c c . . 
+        . . . . c b 5 5 d c b b b c . . 
+        `)
+    animWalkRight.addAnimationFrame(img`
+        . . . . . . . c c c c c . . . . 
+        . . . . . . c 5 5 5 5 5 c c . . 
+        . . . . . c 5 5 f 1 5 5 5 5 c . 
+        . . . . c 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 b 5 5 5 5 c . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c d d d b 5 5 b c c c . . . . 
+        . . c c c b b 5 5 d c . . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . . c b b b c . . . . 
+        `)
+    animWalkRight.addAnimationFrame(img`
+        . . . . . . . c c c c c . . . . 
+        . . . . . . c 5 5 5 5 5 c c . . 
+        . . . . . c 5 5 f 1 5 5 5 5 c . 
+        . . . . c 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 5 c 
+        . . c d d d 5 5 5 b 5 5 5 5 c . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        . c c d d d b 5 5 b c c . . . . 
+        . . . c c c b b 5 5 d c . . . . 
+        . . . . . c c c c c c c . . . . 
+        . . . . . . . c b b b c . . . . 
+        `)
+    animWalkRight.addAnimationFrame(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 b b 3 3 c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        c c d d d b 5 5 d c c c c . . . 
+        . . c c c b 5 5 b c c b c . . . 
+        . . . . . c b 5 5 d c c c . . . 
+        `)
+    animation.attachAnimation(dino, animWalkLeft)
+    animation.attachAnimation(dino, animWalkRight)
+}
+function createEggs () {
+    tile_list = scene.getTilesByType(5)
+    for (let value of tile_list) {
+        Egg = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . f f f . . . . . . . . 
+            . . . . f 1 5 5 f . . . . . . . 
+            . . . f 1 5 5 5 5 f . . . . . . 
+            . . . f 5 5 5 5 5 f . . . . . . 
+            . . f 5 5 5 5 5 5 5 f . . . . . 
+            . . f 5 5 5 5 5 5 5 f . . . . . 
+            . . f 5 5 5 5 5 5 5 f . . . . . 
+            . . f 5 5 5 5 5 5 5 f . . . . . 
+            . . f 5 5 5 5 5 5 5 f . . . . . 
+            . . . f 5 5 5 5 5 f . . . . . . 
+            . . . . f f f f f . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Food)
+        scene.place(value, Egg)
+        animation.runMovementAnimation(
+        Egg,
+        animation.animationPresets(animation.bobbing),
+        2000,
+        true
+        )
+    }
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.setAction(dino, ActionKind.Walking)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy()
+    music.jumpUp.play()
+})
+function createDino () {
+    dino = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c 5 5 5 5 c c . . 
+        . . . . . c 5 5 5 5 5 5 5 5 c . 
+        . . . . c 5 5 5 f 1 5 5 5 5 5 c 
+        . . . c 5 5 5 5 f f 5 5 5 5 5 c 
+        . . . c 5 5 5 5 5 5 b 1 b b c c 
+        . . c d 5 5 5 5 5 5 b b 3 3 c c 
+        . . c d d d 5 5 5 5 5 3 3 3 5 c 
+        . . c d d d 5 5 5 5 5 5 5 5 b . 
+        . . c d d d d b 5 5 c b b c . . 
+        c c c d d d d b b 5 5 c b b c . 
+        c d d d d d d d d c c c c c c . 
+        c c d d d b 5 5 d c c c c . . . 
+        . . c c c b 5 5 b c c b c . . . 
+        . . . . . c b 5 5 d c c c . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(dino, 50, 0)
+    dino.ay = 290
+    scene.placeOnRandomTile(dino, 9)
+    scene.cameraFollowSprite(dino)
+    createStandAnimation()
+    createWalkingAnimation()
+    animation.setAction(dino, ActionKind.Walking)
+}
 scene.onHitTile(SpriteKind.Player, 2, function (sprite) {
     game.over(false)
 })
 let Egg: Sprite = null
 let tile_list: tiles.Tile[] = []
+let animWalkRight: animation.Animation = null
+let animWalkLeft: animation.Animation = null
 let dino: Sprite = null
 let animStandLeft: animation.Animation = null
 let animStandRight: animation.Animation = null
